f5417ac1d945cef71cb4d6209372893cb42f059c
Asutosh Agrawal <83363716+ashuagr@users.noreply.github.com>
2024-06-30 23:39:07 +0530

    Create 3. Write a C program to print “MySirG” on the screen.c

---
+++ Assignment 4/3. Write a C program to print “MySirG” on the screen.c
@@ -0,0 +1,11 @@
+/* 3.Write a C program to print “MySirG” on the screen.*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+    printf("MySirG");
+    getch();
+    return 0;
+}
