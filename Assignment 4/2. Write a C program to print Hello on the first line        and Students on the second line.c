a512e77f8c795069e0fc949a7dc60a57e29a0cda
Asutosh Agrawal <83363716+ashuagr@users.noreply.github.com>
2024-06-30 23:36:50 +0530

    Create 2. Write a C program to print Hello on the first line and Students on the second line.c

---
+++ Assignment 4/2. Write a C program to print Hello on the first line        and Students on the second line.c
@@ -0,0 +1,14 @@
+/* 2. Write a C program to print Hello on the first line 
+      and Students on the second line. */
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch;
+    printf("Hello\n");
+    printf("Students");
+    getch();
+    return 0;
+}
