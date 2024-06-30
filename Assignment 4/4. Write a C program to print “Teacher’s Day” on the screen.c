8b4a03ed800610f84e992c5f02bcd2bb84978300
Asutosh Agrawal <83363716+ashuagr@users.noreply.github.com>
2024-06-30 23:40:42 +0530

    Create 4. Write a C program to print “Teacher’s Day” on the screen.c

---
+++ Assignment 4/4. Write a C program to print “Teacher’s Day” on the screen.c
@@ -0,0 +1,12 @@
+ /* 4.Write a C program to print “Teacher’s Day” on the screen.*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch;
+    printf("Teacher's Day");
+    getch();
+    return 0;
+}
